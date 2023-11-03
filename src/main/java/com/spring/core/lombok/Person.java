31ae0d4ca3dccdbb61a89550944d189ad8b90985
wonhwo <dlgy9714@gmail.com>
2023-11-03 11:45:03 +0900

    스프링 기초

---
+++ src/main/java/com/spring/core/lombok/Person.java
@@ -0,0 +1,14 @@
+package com.spring.core.lombok;
+
+import lombok.*;
+//@EqualsAndHashCode
+//@Getter
+//@Setter
+//@ToString
+//@AllArgsConstructor // 모든파라미터 초기화 생성자
+//@NoArgsConstructor //기본 생성자
+public class Person {
+    private  String name;
+    private  int age;
+
+}
---
+++ src/test/java/com/spring/core/lombok/PersonTest.java
@@ -0,0 +1,20 @@
+package com.spring.core.lombok;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+//junit5 : 단위테스트 프레임 워크 - public 붙이면 안됨
+//junit4 : 같은건데 이전버전
+class PersonTest {
+    @Test
+    void lombokTest(){
+        Person person = new Person();
+        person.setName("김철수");
+        person.setAge(30);
+
+        String name = person.getName();
+        System.out.println("person = " + person);
+        new Person("박영희",35);
+    }
+
+}
